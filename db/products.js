d21893c9411260631088862a6ff1ce53cac4de60
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-04-01 11:30:15 -0300

    add a new endpoint for querying an product

--- db/products.js
+++ db/products.js
@@ -1,11 +1,23 @@
 "use strict";
 const client = require("../config/db");
 
-exports.getProducts = async (products) => {
+exports.getAllProducts = async () => {
   let sql =
     `SELECT * FROM products order by calendar_dt DESC`;
   try {
-    let res = await client.query(sql, products)
+    let res = await client.query(sql)
+    return res.rows
+  } catch (err) {
+    console.log(err.stack)
+    return false
+  }
+};
+
+exports.getProductByDay = async (key) => {
+  let sql =
+    `SELECT * FROM products WHERE "key" = '${key}'`;
+  try {
+    let res = await client.query(sql)
     return res.rows
   } catch (err) {
     console.log(err.stack)
--- routes/products.js
+++ routes/products.js
@@ -3,8 +3,15 @@ var router = express.Router();
 let db = require('../db/products')
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  let products = await db.getProducts()
+  let products = await db.getAllProducts()
   console.log(products)
+  res.json(products)
+});
+
+router.get('/item/:key', async function(req, res, next) {
+  let key = req.params.key
+  let products = await db.getProductByDay(key)
+  console.log(key)
   res.json(products)
 });
 
