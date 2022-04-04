8e35bcbcc8b74b7d64dae950a27ba6286b0a441f
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-04-04 18:23:51 -0400

    add cors

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
 
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
 
