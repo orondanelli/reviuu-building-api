7e7dd3381cd813a3d611572d6ae5548b8bd59e1f
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-04-05 12:44:58 -0400

    adding cors :ambulance:

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors')
 var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
 
@@ -13,12 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+app.use(cors());
   
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
