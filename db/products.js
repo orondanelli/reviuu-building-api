c97497c435ef7a4fc46051c22e62c831a0b43b2f
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-04-12 15:06:36 -0400

    :sparkles: add a new property, variation between actual and previous price

--- db/products.js
+++ db/products.js
@@ -3,14 +3,29 @@ const client = require("../config/db");
 
 exports.getAllProducts = async () => {
   let sql =
-    `SELECT 
-    to_char(calendar_dt , 'YYYY-MM-DD') as calendar_dt,
-      "name",
-      "key",
-      src,
-      price,
-      origin    
-    FROM products order by calendar_dt DESC`;
+    `WITH priceR AS (
+      SELECT 
+        to_char(calendar_dt , 'YYYY-MM-DD') as calendar_dt ,
+        "key",  
+        "name",
+        src,
+        brand,
+        origin,
+        price as actual_price  ,
+        lead(price) over (partition by "key" order by calendar_dt DESC) as previous_price  
+      FROM products 
+      ) select
+        calendar_Dt,
+        "key",  
+        "name",
+        src,
+        brand,
+        origin,
+        coalesce(previous_price, actual_price) as previous_price,
+        actual_price,
+      coalesce(TRUNC(((actual_price - previous_price)/actual_price  ::NUMERIC)*100::numeric,1),0) as var  
+      from priceR where "key" IN ('194262999') order by calendar_dt ASC
+      `;
   try {
     let res = await client.query(sql)
     return res.rows
